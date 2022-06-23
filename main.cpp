39ef74da9078fc0b06398c1a7ab0121678c952e7
mleykhner <mleykhner@ya.ru>
2022-06-23 22:05:52 +0300

    Вращение рефлектора все еще не работает, но ворнингов уже нет. Также добавлен плагборд

--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 #include "configuration.cpp"
 #include "string"
 
-int rotorStep(int rotor, int input, int direction, int position);
-int projectLetter(int input, int rots[], int * poss);
-void newPositions(int * positions);
-std::string encryptText(std::string input, int rots[], int * poss);
+int rotorStep(int rotor, int input, int direction, int position);//Проецирование символа через ротор
+int projectLetter(int input, int rots[], int * poss); //Проецирование символа через все роторы
+void newPositions(int * positions); //Расчет новых положений роторов
+std::string encryptText(std::string input, int rots[], int * poss); //Шифрование строки
+void updatePlugboardSetup(char A, char B); //Изменение настройки коммутационной панели
+void resetPlugboardSetup(); //Сбросить настройки коммутационной панели
 
 int main() {
     int activeRotors[] = {0, 1, 2, 5}; //Настройка активных роторов и рефлектора
     int activeRotors_poss[] = {11, 23, 12, 0}; //Настройка начального положения //Изменение позиции рефлектора ломает все
 
-    std::string text = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    std::string text = "HGA";
     std::string encrypted = encryptText(text, activeRotors, activeRotors_poss);
     std::cout << encrypted;
     return 0;
@@ -19,7 +21,7 @@ int main() {
 
 std::string encryptText(std::string input, int rots[], int * poss){
     bool unencryptedSymbols = false;
-    int length = input.length();
+    int length = int(input.length());
     std::string temp;
     for(int i = 0; i < length; i++){
         int currentSymbol = int(toupper(input[i])) - 65;
@@ -40,16 +42,17 @@ std::string encryptText(std::string input, int rots[], int * poss){
 
 int rotorStep(int rotor, int input, int direction, int position){
     return (rotors[rotor][direction][(input + (direction ? 0 : position)) % 26] - (direction ? position : 0)) % 26;
+    //            ^ротор ^направление   ^входной символ      ^смещение вперед                           ^смещение назад
 }
 
 int projectLetter(int input, int rots[], int * poss){
-    int temp = input;
+    int temp = plugboardSetup[input];
     for (int i = 3; i > -4 ; i--){
         temp = rotorStep(rots[3 - abs(i)], temp, (i >= 0 ? 0 : 1), poss[3 - abs(i)]);
         while (temp < 0) temp += 26;
     }
     newPositions(poss);
-    return temp;
+    return plugboardSetup[temp];
 }
 
 void newPositions(int * positions){
@@ -59,5 +62,16 @@ void newPositions(int * positions){
             positions[i] = 0;
             if(i < 3) (positions[i + 1])++;
         }
+    }
+}
+
+void updatePlugboardSetup(char A, char B){
+    plugboardSetup[int(A) - 65] = int(B) - 65;
+    plugboardSetup[int(B) - 65] = int(A) - 65;
+}
+
+void resetPlugboardSetup() {
+    for (int i = 0; i < 26; i++){
+        plugboardSetup[i] = i;
     }
 }
