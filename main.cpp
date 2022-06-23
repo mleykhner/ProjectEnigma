83559d6105cd9515c66e4e5db4552664f3c65dd4
mleykhner <mleykhner@ya.ru>
2022-06-23 19:04:09 +0300

    Исправлена ошибка с длиной шифруемой строки. Ворнинги еще остались

--- main.cpp
+++ main.cpp
@@ -1,49 +1,63 @@
 #include <iostream>
 #include "configuration.cpp"
+#include "string"
 
 int rotorStep(int rotor, int input, int direction, int position);
-void calculatePositions(int * rotA, int * rotB, int * rotC, int * ref);
-int projectChar(int input, int rots[], int poss[]);
+int projectLetter(int input, int rots[], int * poss);
+void newPositions(int * positions);
+std::string encryptText(std::string input, int rots[], int * poss);
 
 int main() {
-    int activeRotors[] = {0, 1, 2, 5};
-    int activeRotors_poss[] = {0, 0, 0, 0};
-    std::cout << char(projectChar(int('N') - 65, activeRotors, activeRotors_poss) + 65) << std::endl;
-    calculatePositions(&activeRotors_poss[0], &activeRotors_poss[1], &activeRotors_poss[2], &activeRotors_poss[3]);
-    std::cout << char(projectChar(int('E') - 65, activeRotors, activeRotors_poss) + 65) << std::endl;
-    calculatePositions(&activeRotors_poss[0], &activeRotors_poss[1], &activeRotors_poss[2], &activeRotors_poss[3]);
+    int activeRotors[] = {0, 1, 2, 5}; //Настройка активных роторов и рефлектора
+    int activeRotors_poss[] = {11, 23, 12, 0}; //Настройка начального положения //Изменение позиции рефлектора ломает все
+
+    std::string text = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    std::string encrypted = encryptText(text, activeRotors, activeRotors_poss);
+    std::cout << encrypted;
     return 0;
 }
 
-int rotorStep(int rotor, int input, int direction, int position){
-        return rotors[rotor][direction][(input + position) % 26];
-}
-
-int projectChar(int input, int rots[], int poss[]){
-    int temp = input;
-    std::cout << char(temp) << " -> ";
-    for (int i = 3; i > -4 ; i--){
-        temp = rotorStep(rots[3 - abs(i)], temp, (i >= 0 ? 0 : 1), poss[3 - abs(i)]);
-        std::cout << char(temp) << " -> ";
+std::string encryptText(std::string input, int rots[], int * poss){
+    bool unencryptedSymbols = false;
+    int length = input.length();
+    std::string temp;
+    for(int i = 0; i < length; i++){
+        int currentSymbol = int(toupper(input[i])) - 65;
+        if(currentSymbol >= 0 && currentSymbol <= 25){
+            temp += char(projectLetter(currentSymbol, rots, poss) + 65);
+        }
+        else if (currentSymbol == -33){
+            temp += ' ';
+        }
+        else{
+            if (currentSymbol != -65) unencryptedSymbols = true;
+            temp += input[i];
+        }
     }
+    if(unencryptedSymbols) std::cout << "String contains unencryptable symbols\n";
     return temp;
 }
 
-void calculatePositions(int * rotA, int * rotB, int * rotC, int * ref){
-    (*rotA)++;
-    if(*rotA > 25){
-        *rotA = 0;
-        (*rotB)++;
+int rotorStep(int rotor, int input, int direction, int position){
+    return (rotors[rotor][direction][(input + (direction ? 0 : position)) % 26] - (direction ? position : 0)) % 26;
+}
+
+int projectLetter(int input, int rots[], int * poss){
+    int temp = input;
+    for (int i = 3; i > -4 ; i--){
+        temp = rotorStep(rots[3 - abs(i)], temp, (i >= 0 ? 0 : 1), poss[3 - abs(i)]);
+        while (temp < 0) temp += 26;
     }
-    if(*rotB > 25){
-        *rotB = 0;
-        (*rotC)++;
-    }
-    if(*rotC > 25){
-        *rotC = 0;
-        (*ref)++;
-    }
-    if(*ref > 25){
-        *ref = 0;
+    newPositions(poss);
+    return temp;
+}
+
+void newPositions(int * positions){
+    (positions[0])++;
+    for (int i = 0; i < 4; i++){
+        if(positions[i] > 25){
+            positions[i] = 0;
+            if(i < 3) (positions[i + 1])++;
+        }
     }
 }
